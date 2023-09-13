e1bb721ac6a9f03ad5ce68b0c610613f81049ca1
rallej01 <113207486+rallej01@users.noreply.github.com>
2023-09-13 16:08:24 +0800

    Update _app.tsx

--- Brisk-Sloth-Staking-main/pages/_app.tsx
+++ Brisk-Sloth-Staking-main/pages/_app.tsx
@@ -7,7 +7,7 @@ const activeChain = "polygon";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={activeChain} clientId="ff036d153ee26cd302df9a29b88792e8">
+    <ThirdwebProvider activeChain={activeChain} clientId="197ced514e391a7579d8935a74177d41">
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
